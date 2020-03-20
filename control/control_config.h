7bfaf63170be5a1b8dbada59262aead00a809288
Guillaume Danny Deng <zxpay73945788@gmail.com>
2020-03-20 20:13:48 +0800

    新增設定control 參數的file

---
+++ control/control_config.h
@@ -0,0 +1,26 @@
+/**
+ * @file control_config.h
+ * @author Xiang-Guan Deng
+ * @brief Config control parameters
+ * @date 2020.xx.xx
+ *
+ */
+
+#ifndef CONTROL_CONFIG_H
+#define CONTROL_CONFIG_H
+
+/* Define parameters */
+#define EXC_KI                0.0          /* 激磁角Ki回饋 */
+#define I_SVPWM_KP            1/360.0      /* 電流Kp回饋 */
+#define I_SVPWM_KI            0.0          /* 電流Ki回饋 */
+#define I_SVPWM_LOW           0.6          /* 電流下限 */
+#define I_SVPWM_HIGH          0.6          /* 電流上限 */
+#define STEP_C_THETA_LENGTH   1.0          /* 命令微步累加器角度轉換成長度增益 */
+#define STEP_S_THETA_LENGTH   1.0          /* 感測微步累加器角度轉換成長度增益 */
+
+
+/* Define default N-step */
+#define N_STEP 1
+
+
+#endif /* CONTROL_CONFIG_H */
--- src/main.c
+++ src/main.c
@@ -19,15 +19,7 @@
 #include "fpu.h"
 #include "MKV30F12810.h"                // NXP::Device:Startup:MKV30F12810_startup
 #include "MKV30F12810_features.h"       // NXP::Device:Startup:MKV30F12810_startup
-
-/* Define parameters */
-#define EXC_KI                0.0         /* 激磁角Ki回饋 */
-#define I_SVPWM_KP            1/360.0      /* 電流Kp回饋 */
-#define I_SVPWM_KI            0.0    /* 電流Ki回饋 */
-#define I_SVPWM_LOW           0.6          /* 電流下限 */
-#define I_SVPWM_HIGH          0.6          /* 電流上限 */
-#define STEP_C_THETA_LENGTH   1.0          /* 命令微步累加器角度轉換成長度增益 */
-#define STEP_S_THETA_LENGTH   1.0          /* 感測微步累加器角度轉換成長度增益 */
+#include "control_config.h"
 
 /* Include other library */
 #include "hal_tick.h"
@@ -59,9 +51,6 @@ fb_current_t fb_current;                /* 電流回饋 */
 pwmAB_t pwm12;                          /* 1A1B 2A2B PWM */
 extern step_caccumulator_t c_accum;     /* 命令微步累加器, 在ele_angle.c內初始化 */
 extern step_saccumulator_t s_accum;     /* 感測微步累加器, 在ele_angle.c內初始化 */
-
-/* Define default N-step */
-#define N_STEP 1
 
 #define PIT_BUSY  1
 #define PIT_OK    0
