b3b6f62d30873d8ab4b2da6b43a4854383cec204
Guillaume Danny Deng <zxpay73945788@gmail.com>
2020-03-24 14:44:30 +0800

    解除命令電子角累加限制
    所有的角度都可以是修正的值

--- control/i_excite_angle.c
+++ control/i_excite_angle.c
@@ -6,8 +6,8 @@ void init_exc_ang_para(fb_exc_angle_t *fb_exc_angle, float ki) {
     fb_exc_angle->pid.kd = 0;
     fb_exc_angle->th_cum = 0;
     fb_exc_angle->th_er = 0;
-    fb_exc_angle->th_esvpwm = 0;
-    fb_exc_angle->cum_limit = 90.0/fb_exc_angle->pid.ki;
+    fb_exc_angle->th_esvpwm = 0;    
+    fb_exc_angle->cum_limit = 360.0/fb_exc_angle->pid.ki;
     fb_exc_angle->last_er = 0;
 }
 
@@ -16,32 +16,15 @@ void cal_exc_ang_correct(fb_exc_angle_t *fb_exc_angle, float  e_sdegree, float e
     if(abs_float(fb_exc_angle->th_er) > 180) {
         /* e_sdegree - e_cdegree > 180 */
         if(fb_exc_angle->th_er > 0) {
+            /* 馬達落後 */
             fb_exc_angle->th_er = -(360 - e_sdegree + e_cdegree);
         }
         else {
+            /* 馬達領先 */
             fb_exc_angle->th_er = (360 - e_cdegree + e_sdegree);
         }
     }
 
-    /* 過 +- reset */
-    // float corr = 0;
-    // if((fb_exc_angle->th_er > 0) && (fb_exc_angle->last_er < 0)) {
-    //     if(fb_exc_angle->th_cum < 0) {
-    //         corr = 2*fb_exc_angle->th_er;
-    //         if(-fb_exc_angle->th_cum > corr) fb_exc_angle->th_cum = 0;
-    //         else fb_exc_angle->th_cum += 1.5*fb_exc_angle->th_er;
-    //     }
-    // }
-    // else if((fb_exc_angle->th_er < 0) && (fb_exc_angle->last_er > 0)){
-    //     if(fb_exc_angle->th_cum > 0) {
-    //         corr = 2*fb_exc_angle->th_er;
-    //         if(fb_exc_angle->th_cum > -corr) fb_exc_angle->th_cum = 0;
-    //         else fb_exc_angle->th_cum += 1.5*fb_exc_angle->th_er;
-    //     }
-    // }
-    // else {
-    //     fb_exc_angle->th_cum += fb_exc_angle->th_er;     /* 累計誤差 */
-    // }
     fb_exc_angle->th_cum += fb_exc_angle->th_er;     /* 累計誤差 */
 
     /* 限制上下界 */
@@ -52,6 +35,7 @@ void cal_exc_ang_correct(fb_exc_angle_t *fb_exc_angle, float  e_sdegree, float e
     /* 計算 theta_svpwm 值 (角差I回饋) */
     fb_exc_angle->th_esvpwm = e_cdegree - fb_exc_angle->pid.ki*fb_exc_angle->th_cum;
 
+    /* theta svpwm to positive */
     if(fb_exc_angle->th_esvpwm > 360)  fb_exc_angle->th_esvpwm -= 360;
     if(fb_exc_angle->th_esvpwm < -360) fb_exc_angle->th_esvpwm += 360;
 
--- control/svpwm.c
+++ control/svpwm.c
@@ -10,34 +10,7 @@ void cal_pwmAB(pwmAB_t *pwmAB, fb_exc_angle_t *fb_exc_angle, fb_current_t *fb_cu
     /* 由角差I回饋 和 電流PI回饋計算SVPWM */
     float ia_svpwm = (-fb_current->i_svpwm)*sin(fb_exc_angle->th_esvpwm*DEGREE_TO_RADIAN);
     float ib_svpwm = (-fb_current->i_svpwm)*cos(fb_exc_angle->th_esvpwm*DEGREE_TO_RADIAN);
-    // float ia = sin(fb_exc_angle->th_esvpwm*DEGREE_TO_RADIAN);
-    // float ib = cos(fb_exc_angle->th_esvpwm*DEGREE_TO_RADIAN);
-
-    /* 設定i_a、i_b上下界 */
-    // if(i_a > 0) {
-    //     if(i_a > Ia_MAX) {
-    //         i_a = Ia_MAX;
-    //     }
-    // }
-    // else {
-    //     if(i_a < -Ia_MAX) {
-    //         i_a = -Ia_MAX;
-    //     }
-    // }
-    // if(i_b > 0) {
-    //     if(i_b > Ib_MAX) {
-    //         i_b = Ib_MAX;
-    //     }
-    // }
-    // else {
-    //     if(i_b < -Ib_MAX) {
-    //         i_b = -Ib_MAX;
-    //     }
-    // }
 
     pwmAB->pwm1 = ((int32_t)(ia_svpwm * PERIOD_COUNT) + PERIOD_COUNT) >> 1;
     pwmAB->pwm2 = ((int32_t)(ib_svpwm * PERIOD_COUNT) + PERIOD_COUNT) >> 1;
-
-    // pwmAB->pwm1 = ia*PERIOD_COUNT;
-    // pwmAB->pwm2 = ib*PERIOD_COUNT;
 }
