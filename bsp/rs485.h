2cfefa1cc42e8c69d5d93d041b037f0b9a5555c9
Guillaume Danny Deng <zxpay73945788@gmail.com>
2020-04-27 16:27:11 +0800

    更新RS485 macro名子

--- bsp/rs485.h
+++ bsp/rs485.h
@@ -14,7 +14,7 @@ extern char send_buf[SEND_BUFF_SIZE];
 #define DMA_IDLE_BUT_FAIL              3
 #define DMA_MISS_DATA                  4
 #define RS485_DMA_Handler              DMA0_IRQHandler
-#define RS485_trm(format, args...)     rs485_send_edma(sprintf(send_buf, "[RS485] "format, ##args));  \
+#define RS485_trm(format, args...)     rs485_send_edma(sprintf(send_buf, "[485],"format, ##args));  \
                                        /* Disable when transmit complete */
 
 void rs485_send_edma(size_t buf_sz);
@@ -25,7 +25,7 @@ void clear_dma_error(void);
 
 #else
 #define RS485_trm(format, args...)     ENABLE_RS485_TRM();                                        \
-                                       printf("[RS485] "format, ##args);                          \
+                                       printf("[485],"format, ##args);                          \
                                        RS485_UART->C2 |= UART_C2_TCIE_MASK;
 #endif /* USE_UART_DMA */
 
